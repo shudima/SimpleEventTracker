c716047f9b884baed7a4cfd641005af29ed517ae
Dima Shulga <dima@365Scores.com>
2015-01-12 19:15:48 +0200

    die

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
 root 'events#index'
 get 'events' => 'events#index'
-get 'events/' = => 'events#index'
-get 'events/:query/' = => 'events#index'
+get 'events/'  => 'events#index'
+get 'events/:query/'  => 'events#index'
 #match '/events/trackEvent/'  => 'events#trackEvent', :via => [:get, :post]
 
 get '/events/trackEvent/'  => 'events#trackEvent' 
