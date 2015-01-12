f92770f0f340231d9c889e0893224d5c97be5405
Dima Shulga <dima@365Scores.com>
2015-01-12 17:23:39 +0200

    add post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
 root 'events#index'
 get 'events' => 'events#index'
-get 'events/trackEvent/'  => 'events#trackEvent'
+match 'events/trackEvent/'  => 'events#trackEvent', :via => [:get, :post]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
